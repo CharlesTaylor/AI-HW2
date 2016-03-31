2ff3684aefd5c7ece7f6b81f5a249056588452db
= <=>
2016-03-31 23:01:27 +0300

    stuff

--- hw2.py
+++ hw2.py
@@ -1,16 +1,24 @@
 
 from random import shuffle
 
+def Manhattan(puzzle):
+	distance = 0
+	for i in range(len(lis)):
+		if(lis[i] is not -1):
+			distance = distance + abs(puzzle[i] % 3 - i % 3) + abs(puzzle[i] / 3 - i /3)
+
+	return distance
+
 def PuzzleGenerator():
-	lis = [0,1,2,3,4,5,6,7,8]
+	lis = [-1,0,1,2,3,4,5,6,7]#Repsresents value+1
 	shuffle(lis)
-	print lis
+	#print lis
 
 	okashina = 0
 	for i in range(len(lis)):
-		if lis[i] is not 0:
+		if lis[i] is not -1:
 			for j in range(i,len(lis)):
-				if lis[j] is not 0:
+				if lis[j] is not -1:
 					if(lis[j] < lis[i]):
 						okashina = okashina + 1
 	if( okashina % 2 is 1):#Not Solvable
@@ -19,17 +27,24 @@ def PuzzleGenerator():
 		return lis
 
 
-def BeamItUp(puzzle, w, ):
-	pass
-
-
-
+def BeamItUp(puzzle, w):
+	
+	l = []
+	ll = []
+	ll.append(puzzle)
+	empty = 0
+	empty = puzzle.index(-1);
+	row = puzzle[empty] /3
+	col = puzzle[empty] %3
+	
+	print empty
 
 
 
 def main():
-	PuzzleGenerator()
-
+	l = PuzzleGenerator()
+	print l
+	BeamItUp(l,3)
 
 
 
