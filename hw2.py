d3146f4606f4cdbdf8331e51b56aef62f0f5f2fb
= <=>
2016-04-02 20:14:05 +0300

    done: move generator

--- hw2.py
+++ hw2.py
@@ -26,13 +26,39 @@ def PuzzleGenerator():
 	else:#Solvable so return
 		return lis
 
-
+def PossibleMoves(puzzle):
+	al = []
+	empty = puzzle.index(-1);
+	row = empty /3
+	col = empty %3
+	print (str(row)+	"-"+str(col))
+	tmp = list(puzzle)
+	if row  > 0:
+		tmp[empty] = tmp[(row -1)*3+col]
+		tmp[(row -1)*3+col] = -1
+		al.append(list(tmp))
+	tmp = list(puzzle)
+	if row +1 < 3:
+		tmp[empty] = tmp[(row +1)*3+col]
+		tmp[(row +1)*3+col] = -1
+		al.append(list(tmp))
+	tmp = list(puzzle)
+	if col > 0:
+		tmp[empty] = tmp[row*3+col-1]
+		tmp[row*3+col-1] = -1
+		al.append(list(tmp))
+	tmp = list(puzzle)
+	if col + 1 < 3:
+		tmp[empty] = tmp[row*3+col+1]
+		tmp[row*3+col+1] = -1
+		al.append(list(tmp))
+	return al
 def BeamItUp(puzzle, w):
 	
 	l = []
 	ll = []
 	ll.append(puzzle)
-	empty = 0
+	
 	empty = puzzle.index(-1);
 	row = puzzle[empty] /3
 	col = puzzle[empty] %3
@@ -44,6 +70,7 @@ def BeamItUp(puzzle, w):
 def main():
 	l = PuzzleGenerator()
 	print l
+	print PossibleMoves(l)
 	BeamItUp(l,3)
 
 
